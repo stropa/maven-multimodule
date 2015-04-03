4777f1aab0510ded7150dc2ef0742d48d9678a50
Deepak Shevani <deepak.shevani@flipkart.com>
2015-04-03 18:59:42 +0530

    Adding basic template for multimodule maven

---
+++ module2/src/main/java/com/flipkart/ekl/App.java
@@ -0,0 +1,13 @@
+package com.flipkart.ekl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
